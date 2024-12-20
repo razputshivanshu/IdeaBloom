8dde774cc9d3d138de62e0f7253039d90d021231
razputshivanshu <razputshivanshu@gmail.com>
2024-12-20 17:54:46 +0530

    gg

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,64 +1,78 @@
-import React, { useState } from 'react';
-import { IKImage } from 'imagekitio-react';
-import Image from './Image';
-import { Link } from 'react-router-dom';
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
+import { useState } from "react";
+import Image from "./Image";
+import { Link } from "react-router-dom";
+import { SignedIn, SignedOut, UserButton } from "@clerk/clerk-react";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className='w-full h-16 md:h-20 flex items-center justify-between'>
-      {/* Logo */}
-      <Link to="/" className='flex items-center gap-4 text-2xl font-bold'>
-        <Image src="logo.png" alt="Main Logo" w={32} h={32} />
-        <span>IdeaBloom</span>
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
+      {/* LOGO */}
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
+        <Image src="logo.png" alt="Lama Logo" w={32} h={32} />
+        <span>lamalog</span>
       </Link>
-
-      {/* Mobile Menu Button */}
-      <div className='md:hidden'>
+      {/* MOBILE MENU */}
+      <div className="md:hidden">
+        {/* MOBILE BUTTON */}
         <div
-          className='cursor-pointer text-4xl'
+          className="cursor-pointer text-4xl"
           onClick={() => setOpen((prev) => !prev)}
         >
-          {open ? 'X' : '☰'}
+          {/* Change Hamburger Icon */}
+          {/* {open ? "X" : "☰"} */}
+          <div className="flex flex-col gap-[5.4px]">
+            <div
+              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${
+                open && "rotate-45"
+              }`}
+            ></div>
+            <div
+              className={`h-[3px] rounded-md w-6 bg-black transition-all ease-in-out ${
+                open && "opacity-0"
+              }`}
+            ></div>
+            <div
+              className={`h-[3px] rounded-md w-6 bg-black origin-left transition-all ease-in-out ${
+                open && "-rotate-45"
+              }`}
+            ></div>
+          </div>
+        </div>
+        {/* MOBILE LINK LIST */}
+        <div
+          className={`w-full h-screen bg-[#e6e6ff] flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 transition-all ease-in-out ${
+            open ? "-right-0" : "-right-[100%]"
+          }`}
+        >
+          <Link to="/" onClick={()=>setOpen(false)}>Home</Link>
+          <Link to="/posts?sort=trending" onClick={()=>setOpen(false)}>Trending</Link>
+          <Link to="/posts?sort=popular" onClick={()=>setOpen(false)}>Most Popular</Link>
+          <Link to="/" onClick={()=>setOpen(false)}>About</Link>
+          <Link to="/login" onClick={()=>setOpen(false)}>
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
+          </Link>
         </div>
       </div>
-
-      {/* Mobile Link List */}
-      <div
-        className={`w-full h-screen flex flex-col items-center justify-center gap-8 font-medium text-lg absolute top-16 left-0 transition-all duration-300 ${open ? 'left-0' : '-left-full'}`}
-      >
-        <Link to="/" className='px-4 py-2'>Home</Link>
-        <Link to="/" className='px-4 py-2'>Trending</Link>
-        <Link to="/" className='px-4 py-2'>Most Popular</Link>
-        <Link to="/" className='px-4 py-2'>About</Link>
-        <Link to="/" className='px-4 py-2'>
-          <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>
-            Login 👋
-          </button>
-        </Link>
-      </div>
-
-      {/* Desktop Menu */}
-      <div className='hidden md:flex items-center gap-8 xl:gap-8 font-medium'>
-        <Link to="/" className='px-4'>Home</Link>
-        <Link to="/" className='px-4'>Trending</Link>
-        <Link to="/" className='px-4'>Most Popular</Link>
-        <Link to="/" className='px-4'>About</Link>
-
-
+      {/* DESKTOP MENU */}
+      <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
+        <Link to="/">Home</Link>
+        <Link to="/posts?sort=trending">Trending</Link>
+        <Link to="/posts?sort=popular">Most Popular</Link>
+        <Link to="/">About</Link>
         <SignedOut>
-        <Link to="/login" className='px-4'>
-          <button className='py-2 px-4 rounded-3xl bg-blue-800 text-white'>
-            Login 👋
-          </button>
+          <Link to="/login">
+            <button className="py-2 px-4 rounded-3xl bg-blue-800 text-white">
+              Login 👋
+            </button>
           </Link>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-        
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
       </div>
     </div>
   );
--- src/routes/HomePage.jsx
+++ src/routes/HomePage.jsx
@@ -24,7 +24,44 @@ const HomePage = () => {
           </p>
         </div>
         {/* Animated Button */}
-        <Link to="write"></Link>
+        <Link to="write" className="hidden md:block relative">
+          <svg
+            viewBox="0 0 200 200"
+            width="200"
+            height="200"
+            // className="text-lg tracking-widest animate-spin animatedButton"
+            className="text-lg tracking-widest animate-spin animatedButton"
+          >
+            <path
+              id="circlePath"
+              fill="none"
+              d="M 100, 100 m -75, 0 a 75,75 0 1,1 150,0 a 75,75 0 1,1 -150,0"
+            />
+            <text>
+              <textPath href="#circlePath" startOffset="0%">
+                Write your story •
+              </textPath>
+              <textPath href="#circlePath" startOffset="50%">
+                Share your idea •
+              </textPath>
+            </text>
+          </svg>
+          <button className="absolute top-0 left-0 right-0 bottom-0 m-auto w-20 h-20 bg-blue-800 rounded-full flex items-center justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              width="50"
+              height="50"
+              fill="none"
+              stroke="white"
+              strokeWidth="2"
+            >
+              <line x1="6" y1="18" x2="18" y2="6" />
+              <polyline points="9 6 18 6 18 15" />
+            </svg>
+          </button>
+        </Link>
+     
       </div>
 
       {/* Featured Post */}
